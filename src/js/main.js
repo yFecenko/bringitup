9700831d2df72acf8e2743ea379363c22cfbc08b
Yana <nanaha28@gmail.com>
2023-09-09 18:17:28 +0300

    forms, validation, mask

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import MainSlider from './modules/slider/slider-main';
 import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
+import Forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({
@@ -41,4 +42,8 @@ window.addEventListener('DOMContentLoaded', () => {
     player.init();
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
+
+    new Forms('.form').init();
+
+
 });
